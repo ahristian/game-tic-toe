30abe1fd5bebeca13f778a89c5fd54603a02b960
ahristian <hristian.andrei@gmail.com>
2022-12-11 22:02:55 -0700

    some cleaning for router and template

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,23 +5,10 @@ export default class IndexRoute extends Route {
   @service store;
 
   async model() {
-    const game = [
-      { id: 0, value: '' },
-      { id: 1, value: '' },
-      { id: 2, value: 'd' },
-      { id: 3, value: '' },
-      { id: 4, value: '' },
-      { id: 5, value: '' },
-      { id: 6, value: '' },
-      { id: 7, value: '' },
-      { id: 8, value: '' },
-    ];
-
     let new_user = this.store.createRecord('user');
     let users = await this.store.findAll('user');
 
     return {
-      game,
       new_user,
       users,
     };
