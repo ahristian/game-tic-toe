21a80f1aa5ff006058245d0f5df89e71424e93bb
ahristian <hristian.andrei@gmail.com>
2022-12-11 20:48:43 -0700

    revert to initial commit

--- app/components/game.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class GameComponent extends Component {
-  @action async onCellClick(id, cell) {
-    await this.args.onCellClick(id, cell);
-  }
-}
--- app/components/restart-game.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class RestartGameComponent extends Component {
-  @action async onRestartGame() {
-    await this.args.onRestartGame();
-  }
-}
